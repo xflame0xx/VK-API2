e46765eea6a26bb545fd7301559646050de1adc9
ilya <xflame0xx@gmail.com>
2026-01-26 13:00:12 +0700

    Update router.js

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 // router.js
-import { MainPage } from "../Pages/MainPage.js";
-import { UserPage } from "../Pages/UserPage.js";
+import { MainPage } from "./Pages/MainPage.js";
+import { UserPage } from "./Pages/UserPage.js";
 
 export class Router {
   constructor(root) {
